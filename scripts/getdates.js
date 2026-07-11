47d3157449c4e9f48b2ea543ec1e67ba4891ce25
barringtonm <bmiller4@byupathway.edu>
2026-07-11 19:59:21 -0400

    change

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -3,5 +3,5 @@ const currentYearElement = document.getElementById("currentYear");
 currentYearElement.textContent = new Date().getFullYear();
 
 //Last Modified Date//
-const LastModifiedElement = document.getElementById("lastModified");
+const lastModifiedElement = document.getElementById("lastModified");
 lastModifiedElement.textContent = "Last Modified: " + document.lastModified;
